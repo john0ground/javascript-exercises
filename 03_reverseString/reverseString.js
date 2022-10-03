ce2a2531a441c6b233daeab8593da54532441edb
John <mjohncorbet@gmail.com>
2022-10-03 18:47:51 +0800

    place-the-odin-project-solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -7,5 +7,10 @@ const reverseString = function(str) {
     return result;
 };
 
+// const reverseString = function(string) {
+//     return string.split('').reverse().join('');
+//    };
+   
+
 // Do not edit below this line
 module.exports = reverseString;
