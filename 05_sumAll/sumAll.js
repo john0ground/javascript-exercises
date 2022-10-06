e25cb8615ab32b2f4ac9bf965615cd669fe9b4a8
John <mjohncorbet@gmail.com>
2022-10-06 19:29:58 +0800

    works with larger number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,19 @@
 const sumAll = function(int1, int2) {
     let sum = 0;
 
-    for(i = int1; i <= int2; i++) {
-        sum += i;
+    if(int1 > int2) {
+        for(i = int1; i >= int2; i--) {
+            sum += i;
+        };
+    } else {
+        for(i = int1; i <= int2; i++) {
+            sum += i;
+        };
     }
-
+    
     return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
