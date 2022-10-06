e1b8a86921add43ea0c6c9896c6e2c6392b2e979
John <mjohncorbet@gmail.com>
2022-10-06 20:22:59 +0800

    final solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,15 @@
 const sumAll = function(int1, int2) {
     let sum = 0;
 
-    if(int1 > int2) {
+    if(int1 < 0 
+       || int2 < 0  
+       || isNaN(int1) 
+       || isNaN(int2)
+       || typeof(int1) == 'string'
+       || typeof(int2) == 'string') {
+        sum = 'ERROR';
+
+    } else if(int1 > int2) {
         for(i = int1; i >= int2; i--) {
             sum += i;
         };
@@ -14,6 +22,7 @@ const sumAll = function(int1, int2) {
     return sum;
 };
 
+// console.log(sumAll(3, -2));
 
 // Do not edit below this line
 module.exports = sumAll;
