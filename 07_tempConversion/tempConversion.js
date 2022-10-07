86ebf0371846f2c7c0b075564c49a553637507d4
John <mjohncorbet@gmail.com>
2022-10-07 20:47:35 +0800

    my solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    const ftocConvert = (temp - 32) * (5/9);
+    const ftocResult = Math.round(ftocConvert * 10) / 10;
+    return ftocResult;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const ctofConvert = temp * (9/5) + 32;
+  const ctofResult = Math.round(ctofConvert * 10) / 10;
+  return ctofResult;
 };
 
 // Do not edit below this line
