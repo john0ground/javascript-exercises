006879c59bbacb6c5b20983749c3ac39b64e6714
John <mjohncorbet@gmail.com>
2022-10-13 20:46:20 +0800

    my solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    
+    if (typeof(member) == 'string') {
+        member = Number(member);
+    } else if (member < 0) {return 'OOPS';}
+    
+    series = [1, 1];
+    for (i=0; i < 30; i++) {
+        n = (series[series.length - 1]) + (series[series.length - 2]);
+        series.push(n);
+    }
 
+    return series[member - 1]
 };
 
 // Do not edit below this line
