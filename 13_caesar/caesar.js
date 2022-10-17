29050c978fddef62ddef88ec8b8899a9f9cf10a4
John <mjohncorbet@gmail.com>
2022-10-17 22:05:58 +0800

    first solution(works with letters)

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,26 @@
-const caesar = function() {
+const caesar = function(...args) {
+    const str = args[0].split('');
+    const shift = args[1];
 
+    const newStr = str.map((item) => {
+        let num = item.charCodeAt(item) + shift;
+        return String.fromCharCode(num);
+    });
+    return newStr.join('');
+    
+    // for (let i = 0; i < str.length; i++) {
+    //     if (str[i].match(/[!,.';?" ]/)) {
+    //         convertedStr.push(str[i]);
+    //     }
+    //     else {
+    //     let num = str.charCodeAt(i) + shift;
+    //     convertedStr.push(String.fromCharCode(num));
+    //     }
+    // }
+
+    return convertedStr.join('');
 };
+console.log(caesar('Dog', -1));
 
 // Do not edit below this line
 module.exports = caesar;
