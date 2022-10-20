d863b0a783452a7b831082ab8a1eee6702dcc2cd
John <mjohncorbet@gmail.com>
2022-10-20 20:47:28 +0800

    add comments for solution

--- 13_caesar/solutionTOP.js
+++ 13_caesar/solutionTOP.js
@@ -1,3 +1,10 @@
+// 1. create function to array the string,
+//    then map function that returns the METHOD,
+//    then combine it to string
+
+// 2. METHOD for shifting (shiftChar)
+
+
 const caesar = function(string, shift) {
     return string
       .split("")
@@ -5,6 +12,7 @@ const caesar = function(string, shift) {
       .join("");
   };
   
+  // base numbers to add shifts later
   const codeSet = code => (code < 97 ? 65 : 97);
   
   // this function is just a fancy way of doing % so that it works with negative numbers
@@ -12,9 +20,12 @@ const caesar = function(string, shift) {
   // https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
   const mod = (n, m) => (n % m + m) % m;
   
+
+
   const shiftChar = (char, shift) => {
     const code = char.charCodeAt();
   
+    // shift only alphabets
     if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
       return String.fromCharCode(
         mod(code + shift - codeSet(code), 26) + codeSet(code)
